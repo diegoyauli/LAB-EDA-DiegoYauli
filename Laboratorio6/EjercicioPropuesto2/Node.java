e90d92f6113bea4a71f66f445285d6b6eabd6251
diegoyauli <dyaulim@unsa.edu.pe>
2025-06-16 10:57:04 -0500

    Agregando clase Node genérica para la implementación de estructuras de datos para el problema 2.

---
+++ Laboratorio6/EjercicioPropuesto2/Node.java
@@ -0,0 +1,17 @@
+package Laboratorio6.EjercicioPropuesto2;
+
+public class Node<T> {
+    T data;
+    Node<T> left;
+    Node<T> right;
+
+    public Node(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
